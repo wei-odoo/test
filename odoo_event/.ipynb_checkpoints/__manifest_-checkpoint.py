c386875fe0f76e64fe5fdb99f0de405eaca973ef
OdooWe <odoo@eupd02.odoo.com>
2022-10-06 07:56:27 +0000

    [ADD] Basic Module

---
+++ odoo_event/.ipynb_checkpoints/__manifest_-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Event',
+    'summary': """Event manager""".
+    'description':""""
+        Module to manege events
+        - trainings
+        - Concerts
+        - webinar
+    """,
+    'author': 'OdooWe',
+    'website': 'https://www.event-odoo.com',
+    'category': 'Event',
+    'version': '0.0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo':[
+        
+    ],
+}
